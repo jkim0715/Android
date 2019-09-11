1b2aa46755707276e110e86dfc75a21aab94963c
jkim0715 <gjr191@naver.com>
2019-09-11 11:38:55 +0900

    Add GPS

--- P673/app/src/main/java/com/example/p673/MainActivity.java
+++ P673/app/src/main/java/com/example/p673/MainActivity.java
@@ -1,14 +1,100 @@
 package com.example.p673;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.PermissionChecker;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textview);
+
+        //Give permission for getting location info
+        String[] permissions = {
+                Manifest.permission.ACCESS_FINE_LOCATION
+        };
+        ActivityCompat.requestPermissions(this, permissions, 101);
+
+        int check = PermissionChecker.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (check == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Accept", Toast.LENGTH_SHORT).show();
+            startLocationService();
+            return;
+        } else {
+            Toast.makeText(this, "권한 부여가 안되어 있습니당", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+    }
+
+    public void startLocationService() {
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (location != null) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                String message = "최근 위치 -> Latitude : " + latitude + "\nLongitude:" + longitude;
+
+                textView.setText(message);
+            }
+
+            GPSListener gpsListener = new GPSListener();
+            long minTime = 10000;
+            float minDistance = 0;
+
+            manager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    minTime, minDistance, gpsListener);
+
+            Toast.makeText(getApplicationContext(), "내 위치확인 요청함",
+                    Toast.LENGTH_SHORT).show();
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class GPSListener implements LocationListener {
+        public void onLocationChanged(Location location) {
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+
+            String message = "내 위치 -> Latitude : " + latitude + "\nLongitude:" + longitude;
+            textView.setText(message);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+
     }
 }
